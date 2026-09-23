88a901b9dd0aa7ceedf51e3b12112c17074c6b3b
Kanat Zarip Nurkhanuly <zaripkanat17@gmail.com>
2026-09-23 22:01:31 +0500

    Task 6

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,33 @@
+//first step
+
+/*fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val result = firstNumber + secondNumber
+    println("$firstNumber + $secondNumber = $result")
+}
+*/
+//second step and third step
 fun main() {
-    val baseSalary = 5000
-    val bonusAmount = 1000
-    val totalSalary = "$baseSalary + $bonusAmount"
-    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+
+    val result1 = add(firstNumber,secondNumber)
+    val anotherResult1 = add(firstNumber,thirdNumber)
+    val result2 = subtract(firstNumber, secondNumber)
+    val anotherResult2 =subtract(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result1")
+    println("$firstNumber + $thirdNumber = $anotherResult1")
+    println("$firstNumber - $secondNumber = $result2")
+    println("$firstNumber - $thirdNumber = $anotherResult2")
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun subtract(a: Int, b: Int): Int {
+    return a - b
 }
