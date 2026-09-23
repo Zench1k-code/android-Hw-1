85725304471126f53e1684306b86fb2d7b29fc83
Kanat Zarip Nurkhanuly <zaripkanat17@gmail.com>
2026-09-23 22:23:34 +0500

    Task 7

--- src/Main.kt
+++ src/Main.kt
@@ -1,33 +1,38 @@
 //first step
-
-/*fun main() {
-    val firstNumber = 10
-    val secondNumber = 5
-    val result = firstNumber + secondNumber
-    println("$firstNumber + $secondNumber = $result")
-}
-*/
-//second step and third step
+/*
 fun main() {
-    val firstNumber = 10
-    val secondNumber = 5
-    val thirdNumber = 8
+    val operatingSystem = "Chrome OS"
+    val emailID = "sample@gmail.com"
 
-    val result1 = add(firstNumber,secondNumber)
-    val anotherResult1 = add(firstNumber,thirdNumber)
-    val result2 = subtract(firstNumber, secondNumber)
-    val anotherResult2 =subtract(firstNumber, thirdNumber)
-
-    println("$firstNumber + $secondNumber = $result1")
-    println("$firstNumber + $thirdNumber = $anotherResult1")
-    println("$firstNumber - $secondNumber = $result2")
-    println("$firstNumber - $thirdNumber = $anotherResult2")
+    println(displayAlertMessage(operatingSystem,emailID))
 }
 
-fun add(a: Int, b: Int): Int {
-    return a + b
+fun displayAlertMessage(a: String, b: String): String {
+    return "There's a new sign-in request on $a for your Google Account $b."
+}
+ */
+
+//second step
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
 }
 
-fun subtract(a: Int, b: Int): Int {
-    return a - b
+fun displayAlertMessage(operatingSystem: String = "Unknown OS", emailId: String): String {
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+
 }
