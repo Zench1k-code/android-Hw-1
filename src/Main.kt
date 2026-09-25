705a3c101dd708094d043f119a5c8f057fd9dd1e
Kanat Zarip Nurkhanuly <zaripkanat17@gmail.com>
2026-09-25 23:28:56 +0500

    Task 10

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,13 @@
-fun compareScreenTime(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
-    return timeSpentToday > timeSpentYesterday
+fun main() {
+    printWeather("Ankara", 27, 31, 82)
+    printWeather("Tokyo", 32, 36, 10)
+    printWeather("Cape Town", 59, 64, 2)
+    printWeather("Guatemala City", 50, 55, 7)
 }
 
-fun main() {
-    println(compareScreenTime(timeSpentToday = 300, timeSpentYesterday = 250))
-    println(compareScreenTime(timeSpentToday = 300, timeSpentYesterday = 300))
-    println(compareScreenTime(timeSpentToday = 200, timeSpentYesterday = 220))
+fun printWeather(city: String, low: Int, high: Int, chanceOfRain: Int) {
+    println("City: $city")
+    println("Low temperature: $low, High temperature: $high")
+    println("Chance of rain: $chanceOfRain%")
+    println()
 }
